9de8517e6072a7f52fd74a3e0b061cdd8d2358fd
themahabur <mahabursoikot04@gmail.com>
2026-06-03 15:40:39 +0600

    feat: enhance order creation with detailed item handling and error management

--- src/modules/orders/orders.route.ts
+++ src/modules/orders/orders.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { ordersController } from "./orders.controller";
+import authorize from "../../middleware/authorize";
 
 
 const ordersRoutes=Router();
 
-ordersRoutes.post("/", ordersController.createOrder);
+ordersRoutes.post("/",authorize("ADMIN" , "CUSTOMER") , ordersController.createOrder);
 ordersRoutes.get("/", ordersController.getOrders);
 
 export default ordersRoutes;
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -3,11 +3,12 @@ import { prisma } from "../../lib/prisma";
 interface CreateOrderPayload {
   deliveryAddress: string;
   contactPhone: string;
-  notes?: string ;
+  notes?: string;
 
   items: {
     mealId: string;
     quantity: number;
+    unitPrice: number;
   }[];
 }
 
@@ -15,29 +16,64 @@ const getOrders = async () => {
   return "This is the orders service.";
 };
 
-
 const createOrder = async (orderData: CreateOrderPayload, userId: string) => {
-  await prisma.$transaction(async (prisma) => {
-    const order = await prisma.order.create({
-      data: {
-        customerId: userId,
-        deliveryAddress: orderData.deliveryAddress,
-        contactPhone: orderData.contactPhone,
-        notes: orderData.notes,
-      },
-    });
+  let totalPrice = 0;
 
-    const orderItems = orderData.items.map((item) => ({
-      orderId: order.id,
+  const mealIds = orderData.items.map((item) => item.mealId);
+
+  const meals = await prisma.meal.findMany({
+    where: {
+      id: {
+        in: mealIds,
+      },
+    },
+  });
+
+  const orderItemsData = orderData.items.map((item) => {
+    const meal = meals.find((m) => m.id === item.mealId);
+
+    if (!meal) {
+      throw new Error("Meal not found");
+    }
+
+    const unitPrice = Number(meal.price);
+    const subtotal = unitPrice * item.quantity;
+
+    totalPrice += subtotal;
+
+    return {
       mealId: item.mealId,
       quantity: item.quantity,
-    }));
-
-    await prisma.orderItem.createMany({ data: {orderItems} });
+      unitPrice,
+      subtotal,
+    };
   });
-}
+
+  const order = await prisma.order.create({
+    data: {
+      customerId: userId,
+      totalPrice,
+      deliveryAddress: orderData.deliveryAddress,
+      contactPhone: orderData.contactPhone,
+      notes: orderData.notes ?? null,
+
+      orderItems: {
+        create: orderItemsData,
+      },
+    },
+    include: {
+      orderItems: true,
+    },
+  });
+
+  if (!order) {
+    throw new Error("Failed to create order");
+  }
+
+  return order;
+};
 
 export const ordersService = {
   getOrders,
-  createOrder
+  createOrder,
 };
