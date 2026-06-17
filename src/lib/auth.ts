bfec66ee5d1156e5d6b84c29059bc3fbccbf3599
themahabur <mahabursoikot04@gmail.com>
2026-06-17 17:08:41 +0600

    feat(auth): add baseURL and social provider configuration for Google authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,8 +9,16 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  trustedOrigins: [process.env.FRONTEND_URL!],
   emailAndPassword: {
     enabled: true,
+  },
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   user: {
     additionalFields: {
